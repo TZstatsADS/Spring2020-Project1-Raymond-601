56a006f81e44517e0fecaf3d89e74a917d48c458
Raymond-601 <60190417+Raymond-601@users.noreply.github.com>
2020-02-02 20:36:55 -0500

    Create f.emo.R

---
+++ lib/f.emo.R
@@ -0,0 +1,23 @@
+#This is the function that output the emotion matrix(as a dataframe) for all the song in the input data.frame
+f.emo <- function(dt){
+  emotion_dad <- NULL
+  for(i in 1:dim(dt)[1]){
+    sentences <- syuzhet::get_sentences(dt$stemmedwords[i])
+    emotions <- matrix(emotion(sentences)$emotion,
+                       nrow = length(sentences),
+                       byrow = T)
+    colnames(emotions) <- emotion(sentences[1])$emotion_type
+    emotions <- data.frame(emotions)
+    emotions <- select(emotions,
+                       anticipation,
+                       joy, 
+                       surprise, 
+                       trust, 
+                       anger, 
+                       disgust, 
+                       fear, 
+                       sadness)
+    emotion_dad <- rbind(emotion_dad, emotions)
+  }
+  return(emotion_dad)
+}
